3ffdaf454a9887afd8b3c9d39ddcef94b70b6467
Tomas Torday <tomas.torday@gmail.com>
2024-06-01 15:16:51 +0200

    convert upper to lowercase with -c option

--- renamer/renamer.sh
+++ renamer/renamer.sh
@@ -3,15 +3,18 @@
 # get current working directory
 directory=$(pwd)
 
-# the first colon supresses error messages
+# the first colon before options supresses error messages - here we do not use it
 # d option has arguments - directory path
 # c option for lower-casing the text
 
 while getopts cd: opt; do
   case "$opt" in
-  c) echo "Found the -c option" ;;
+  c)
+    c_option=1
+    ;;
   d)
     echo "Found the -d option with parameter value $OPTARG"
+    # OPTARG is the option argument in this case dir name
     if [[ -d "$OPTARG" ]]; then
       directory="$OPTARG"
     else
@@ -26,24 +29,42 @@ while getopts cd: opt; do
   esac
 done
 
+# # if you use renamer.sh -d /path/to/dir firstarg secondarg
 # shift $((OPTIND - 1))
+
+# echo "First argument after options is $1"
+# echo "Second argument after options is $2"
 
 for file in "$directory"/*; do
   if [ -f "$file" ]; then
     oldbasename=$(basename "$file")
-    # check if basename contains space or underscore
-    if [[ "$oldbasename" == *[_\ ]* ]]; then
-      # here add verbose if condition
-      echo "Processing file: ${oldbasename}"
-      # translate characters underscore and space to dash
-      newbasename=$(basename "$file" | tr "_ " "--")
-      # renaming the file
-
-      mv "$file" "${directory}/${newbasename}"
-      # here add verbose if condition
-      echo "Renamed ${oldbasename} to ${newbasename}"
+    # check if basename contains space or underscore or capital letters
+    if [[ "$c_option" -eq 1 ]]; then
+      if [[ "$oldbasename" == *[_\ ]* ]] || [[ "$oldbasename" == *[A-Z]* ]]; then
+        # here add verbose if condition
+        echo "Processing file: ${oldbasename}"
+        # translate characters underscore and space to dash then from uppercase to lowercase
+        newbasename=$(basename "$file" | tr "_ " "--" | tr "[:upper:]" "[:lower:]")
+        # renaming the file
+        mv "$file" "${directory}/${newbasename}"
+        # here add verbose if condition
+        echo "Renamed ${oldbasename} to ${newbasename}"
+      else
+        echo "File ${oldbasename} does not need to be renamed."
+      fi
     else
-      echo "File ${oldbasename} does not need to be renamed."
+      if [[ "$oldbasename" == *[_\ ]* ]]; then
+        # here add verbose if condition
+        echo "Processing file: ${oldbasename}"
+        # translate characters underscore and space to dash
+        newbasename=$(basename "$file" | tr "_ " "--")
+        # renaming the file
+        mv "$file" "${directory}/${newbasename}"
+        # here add verbose if condition
+        echo "Renamed ${oldbasename} to ${newbasename}"
+      else
+        echo "File ${oldbasename} does not need to be renamed."
+      fi
     fi
   fi
 done
