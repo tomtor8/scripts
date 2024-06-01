ff877867555abcc8e66616f405e702eb49076bd6
Tomas Torday <tomas.torday@gmail.com>
2024-06-01 13:21:30 +0200

    first working renamer without changing case

--- renamer/renamer.sh
+++ renamer/renamer.sh
@@ -6,14 +6,19 @@ directory=$(pwd)
 for file in "$directory"/*; do
   if [ -f "$file" ]; then
     oldbasename=$(basename "$file")
-    # here add verbose if condition
-    echo "Processing file: ${oldbasename}"
-    # translate characters underscore and space to dash
-    newbasename=$(basename "$file" | tr "_ " "--")
-    # renaming the file
+    # check if basename contains space or underscore
+    if [[ "$oldbasename" == *[_\ ]* ]]; then
+      # here add verbose if condition
+      echo "Processing file: ${oldbasename}"
+      # translate characters underscore and space to dash
+      newbasename=$(basename "$file" | tr "_ " "--")
+      # renaming the file
 
-    mv "$file" "$directory/$newbasename"
-    # here add verbose if condition
-    echo "Renamed ${oldbasename} to ${newbasename}"
+      mv "$file" "${directory}/${newbasename}"
+      # here add verbose if condition
+      echo "Renamed ${oldbasename} to ${newbasename}"
+    else
+      echo "File ${oldbasename} does not need to be renamed."
+    fi
   fi
 done
