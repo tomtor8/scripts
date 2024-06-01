a8f9f8d71e0fa1ee19894f5d95e3863f08fcccff
Tomas Torday <tomas.torday@gmail.com>
2024-06-01 14:18:14 +0200

    parse dir argument from cli

--- renamer/renamer.sh
+++ renamer/renamer.sh
@@ -3,6 +3,31 @@
 # get current working directory
 directory=$(pwd)
 
+# the first colon supresses error messages
+# d option has arguments - directory path
+# c option for lower-casing the text
+
+while getopts cd: opt; do
+  case "$opt" in
+  c) echo "Found the -c option" ;;
+  d)
+    echo "Found the -d option with parameter value $OPTARG"
+    if [[ -d "$OPTARG" ]]; then
+      directory="$OPTARG"
+    else
+      echo "Directory does not exist. Exiting."
+      exit 1
+    fi
+    ;;
+  *)
+    echo "Unknown option: $opt"
+    exit 1
+    ;;
+  esac
+done
+
+# shift $((OPTIND - 1))
+
 for file in "$directory"/*; do
   if [ -f "$file" ]; then
     oldbasename=$(basename "$file")
