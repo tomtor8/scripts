99fac2c6caed325be31cabc006ea3b9c15ff77a1
Tomas Torday <tomas.torday@gmail.com>
2024-07-16 18:34:24 +0200

    upgrade backup to databases

--- backup_databases/budbs.sh
+++ backup_databases/budbs.sh
@@ -1,34 +1,85 @@
 #!/usr/bin/env bash
 
+ensourcefl="/home/tom/Documents/english/en-sk-dict.db"
+entargetfl="/home/tom/Documents/local_backups/en-sk-dict-bak.db"
+husourcefl="/home/tom/Documents/english/hu-sk-dict.db"
+hutargetfl="/home/tom/Documents/local_backups/hu-sk-dict-bak.db"
+booksourcefl="/home/tom/Documents/english/my-books.db"
+booktargetfl="/home/tom/Documents/local_backups/my-books-bak.db"
+pswdsourcefl="/home/tom/Documents/english/t_pswd.kdbx"
+pswdtargetfl="/home/tom/Documents/local_backups/t_pswd-bak.kdbx"
+
+# check modification times of db files
+function backup_is_older() {
+  local source_file=$1
+  local target_file=$2
+  local source_mtime
+  source_mtime=$(stat -c %Y "$source_file")
+  local target_mtime
+  if [[ -f "$target_file" ]]; then
+    target_mtime=$(stat -c %Y "$target_file")
+  else
+    # if target file does not exist time is 0
+    target_mtime=0
+  fi
+  # local diff_secs=$((target_mtime - source_mtime))
+  # check if source is newer than target
+  if [[ $source_mtime -gt $target_mtime ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 if [[ ! -d /home/tom/Documents/local_backups ]]; then
   echo "The local_backups directory does not exist, creating one."
   mkdir /home/tom/Documents/local_backups
 fi
 
-if [[ -f /home/tom/Documents/english/en-sk-dict.db ]]; then
-  sqlite3 /home/tom/Documents/english/en-sk-dict.db ".backup /home/tom/Documents/local_backups/en-sk-dict-bak.db"
-  echo "File en-sk-dict.db backed up in the local_backups directory."
+# check if source files exist
+if [[ -f "$ensourcefl" ]]; then
+  # english slovak database backup
+  if backup_is_older "$ensourcefl" "$entargetfl"; then
+    sqlite3 "$ensourcefl" ".backup ${entargetfl}"
+    echo "File en-sk-dict.db backed up in the local_backups directory."
+  else
+    echo "Backup of the file en-sk-dict.db is not necessary."
+  fi
 else
-  echo "Could not find the en-sk-dict.db file in Documents/english directory."
+  echo "${ensourcefl} does not exist!"
 fi
 
-if [[ -f /home/tom/Documents/english/hu-sk-dict.db ]]; then
-  sqlite3 /home/tom/Documents/english/hu-sk-dict.db ".backup /home/tom/Documents/local_backups/hu-sk-dict-bak.db"
-  echo "File hu-sk-dict.db backed up in the local_backups directory."
+if [[ -f "$husourcefl" ]]; then
+  # hungarian slovak database backup
+  if backup_is_older "$husourcefl" "$hutargetfl"; then
+    sqlite3 "$husourcefl" ".backup ${hutargetfl}"
+    echo "File hu-sk-dict.db backed up in the local_backups directory."
+  else
+    echo "Backup of the file hu-sk-dict.db is not necessary."
+  fi
 else
-  echo "Could not find the hu-sk-dict.db file in Documents/english directory."
+  echo "${husourcefl} does not exist!"
 fi
 
-if [[ -f /home/tom/Documents/english/my-books.db ]]; then
-  sqlite3 /home/tom/Documents/english/my-books.db ".backup /home/tom/Documents/local_backups/my-books-bak.db"
-  echo "File my-books.db backed up in the local_backups directory."
+if [[ -f "$booksourcefl" ]]; then
+  # my books database backup
+  if backup_is_older "$booksourcefl" "$booktargetfl"; then
+    sqlite3 "$booksourcefl" ".backup ${booktargetfl}"
+    echo "File my-books.db backed up in the local_backups directory."
+  else
+    echo "Backup of the file my-books.db is not necessary."
+  fi
 else
-  echo "Could not find the my-books.db file in Documents/english directory."
+  echo "${booksourcefl} does not exist!"
 fi
 
-if [[ -f /home/tom/Documents/english/t_pswd.kdbx ]]; then
-  cp -u /home/tom/Documents/english/t_pswd.kdbx /home/tom/Documents/local_backups/t_pswd-bak.kdbx
-  echo "File t_pswd.kdbx backed up in the local_backups directory."
+if [[ -f "$pswdsourcefl" ]]; then
+  if backup_is_older "$pswdsourcefl" "$pswdtargetfl"; then
+    cp -u "$pswdsourcefl" "$pswdtargetfl"
+    echo "File t_pswd.kdbx backed up in the local_backups directory if ."
+  else
+    echo "Backup of the file t_pswd.kdbx is not necessary."
+  fi
 else
-  echo "Could not find the t_pswd.kdbx file in Documents/english directory."
+  echo "${pswdsourcefl} does not exist!"
 fi
