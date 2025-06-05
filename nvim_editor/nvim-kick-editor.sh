76aeba7d53f1e11f43b19c73f602a2f3d0df7886
Tomas Torday <tomas.torday@gmail.com>
2025-06-05 09:37:52 +0200

    add custom nvim wrapper for EDITOR

---
+++ nvim_editor/nvim-kick-editor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script acts as a wrapper that uses the nvim_kick configuration for $EDITOR environment variable.
+
+# Set the NVIM_APPNAME environment variable for this specific invocation
+NVIM_APPNAME=nvim_kick /usr/bin/nvim "$@"
---
+++ nvim_editor/readme.md
@@ -0,0 +1,4 @@
+# Explanation
+
+- this simple wrapper enables to export $EDITOR or VISUAL environment variables for custom nvim configurations
+- put this script on $PATH (copy this file as nvim-kick-editor)
