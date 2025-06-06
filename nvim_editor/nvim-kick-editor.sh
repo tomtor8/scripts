3975dc0df304a987cd5b229d1cfe19a651d03bce
Tomas Torday <tomas.torday@gmail.com>
2025-06-06 09:04:11 +0200

    change EDITOR to nvim_astro

--- nvim_editor/nvim-kick-editor.sh
+++ nvim_editor/nvim-kick-editor.sh
@@ -2,4 +2,4 @@
 # This script acts as a wrapper that uses the nvim_kick configuration for $EDITOR environment variable.
 
 # Set the NVIM_APPNAME environment variable for this specific invocation
-NVIM_APPNAME=nvim_kick /usr/bin/nvim "$@"
+NVIM_APPNAME=nvim_astro /usr/bin/nvim "$@"
